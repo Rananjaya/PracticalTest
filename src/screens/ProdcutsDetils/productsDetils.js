2fe315a01c1e78733afd8f7f280c65faea459e1e
Rananjaya Bandara <rananjayabandara@gmail.com>
2023-10-13 13:25:14 +0530

    add some redux thing

--- src/screens/ProdcutsDetils/productsDetils.js
+++ src/screens/ProdcutsDetils/productsDetils.js
@@ -1,33 +1,37 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import React, { useEffect } from 'react'
-import HeaderWithDirections from '../../components/headerWithDirections'
-import COLORS from '../../consts/colors'
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import HeaderWithDirections from '../../components/headerWithDirections';
+import COLORS from '../../consts/colors';
 import {useDispatch, useSelector} from 'react-redux';
-import { getAllProductsbyId } from '../../redux/feature/ProductByIdSlice';
+import {getAllProductsbyId} from '../../redux/feature/ProductByIdSlice';
 
-const ProductsDetils = ({navigation,route}) => {
-    const dispatch = useDispatch();
-    const {Product_id} = route.params;
-    console.log("dfsdfdfsd",Product_id)
-    useEffect(()=>{
-        dispatch(getAllProductsbyId({id: Product_id}));
-    },[Product_id])
+const ProductsDetils = ({navigation, route}) => {
+  const dispatch = useDispatch();
+  const {data,isSuccess,loading,message} = useSelector(state => state.getprobyid);
+  const {Product_id} = route.params;
+  console.log('dfsdfdfsd', Product_id);
+  useEffect(() => {
+    dispatch(getAllProductsbyId({id: Product_id}));
+  }, [Product_id]);
   return (
-   <SafeAreaView style={styles.mainView}>
-    <View>
-       <HeaderWithDirections onpress={()=>{
-               navigation.goBack();
-       }}/>
-    </View>
-   </SafeAreaView>
-  )
-}
+    <SafeAreaView style={styles.mainView}>
+      <View>
+        <HeaderWithDirections
+          onpress={() => {
+            navigation.goBack();
+          }}
+        />
+        
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default ProductsDetils
+export default ProductsDetils;
 
 const styles = StyleSheet.create({
-    mainView : {
-        backgroundColor : COLORS.White,
-        flex : 1
-    }
-})
+  mainView: {
+    backgroundColor: COLORS.White,
+    flex: 1,
+  },
+});
